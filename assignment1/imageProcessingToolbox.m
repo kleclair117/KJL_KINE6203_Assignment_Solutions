772bd47af04b3d4dc5e371e361c7feb4c6465420
kleclair117 <leclairk24@students.ecu.edu>
2025-09-10 22:21:14 -0400

    initial commit

---
+++ assignment1/imageProcessingToolbox.m
@@ -0,0 +1,18 @@
+checkOneDriveFileAvailability - read 1 byte of data using BinaryStream to verify if file is available
+                    cmap2gray - Convert colormap to grayscale colormap.
+            getrangefromclass - Get dynamic range of image based on its class.
+                      hsv2rgb - Convert hue-saturation-value colors to red-green-blue.
+                    im2double - Convert image to double precision.
+                      im2gray - Convert RGB image to grayscale.
+                     imresize - Resize image.
+                       imshow - Display image in Handle Graphics figure.
+                       imtile - is a function.
+                      ind2rgb - Convert indexed image to RGB image.
+                      isdicom - Determine if a file is probably a DICOM file.
+                        isdpx - Check if file is DPX.
+                       isnitf - Check if file is NITF.
+                     rgb2gray - Convert RGB image or colormap to grayscale.
+                      rgb2hsv - Convert red-green-blue colors to hue-saturation-value.
+                      rgb2ind - Convert RGB image to indexed image.
+                       images - is a namespace.
+                       matlab - is a namespace.
